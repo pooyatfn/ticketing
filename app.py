dd9dbe55c982d6965293ce67a6746de5f45f35e5
pooya <pooyatoofan@aut.ac.ir>
2023-08-03 00:21:15 +0330

    feat: setting up restful api and sql database.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///templates.db'
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
