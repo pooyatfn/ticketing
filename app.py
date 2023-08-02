70b3c774cfb023cd14ecd78646edc6bb763abc9f
pooya <pooyatoofan@aut.ac.ir>
2023-08-03 00:22:41 +0330

    feat: create Template class.

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///templates.db'
 db = SQLAlchemy(app)
 
+
+class Template(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'category_id': self.category_id
+        }
+
+
 if __name__ == '__main__':
     app.run()
